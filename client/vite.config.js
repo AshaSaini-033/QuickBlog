b5fbdb76c719a1bc2f3763ff892a7e1d183c7626
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-18 00:24:12 +0530

    added tailwind files etc

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss({
+      config: {
+        theme: {
+          extend: {
+            colors: {
+              primary: "#5044E5",
+            },
+            fontFamily: {
+              outfit: ["Outfit", "sans-serif"],
+            },
+          },
+        },
+      },
+    }),
+  ],
 })
