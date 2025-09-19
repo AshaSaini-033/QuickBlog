81ca8e9c08d6fb4b069d0ae86223c8ec33d5cbd9
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-19 11:17:56 +0530

    added sidebar and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Blog from './pages/Blog'
 import Layout from './pages/admin/Layout'
 import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
+import Dashboard from './pages/admin/Dashboard'
+import Comments from './pages/admin/Comments'
 
 const App = () => {
   return (
@@ -12,11 +14,13 @@ const App = () => {
       <Routes>
         <Route path = '/' element = {<Home/>} />
         <Route path = '/blog/:id' element = {<Blog />} />
-         <Route path = '/admin' element={<Layout/>} />
-        <Route index  element={<Layout/>} />
-         <Route path = '/addBlog' element={<AddBlog/>} />
-          <Route path = '/listBlog' element={<ListBlog/>} />
-           <Route path = '/comments' element={<Comment/>} />
+         <Route path = '/admin' element={<Layout/>} >
+        <Route index  element={<Dashboard/>} />
+        {/* childsroutes fro admin pages */}
+         <Route path = 'addBlog' element={<AddBlog/>} />
+          <Route path = 'listBlog' element={<ListBlog/>} />
+           <Route path = 'comments' element={<Comments/>} />
+           </Route>
       </Routes>
     </div>
   )
--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Sidebar from '../../components/admin/Sidebar';
 
 const Layout = () => {
+    const navigate = useNavigate();
+    const logout = ()=>{
+   navigate('/')
+    }
   return (
-    <div>
-      
-    </div>
+   <>
+   {/* navbar of admin page */}
+   <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
+    <img className='w-32 sm:w-40 cursor-pointer' onClick={()=>navigate('/')} src= {assets.logo}/>
+     <button className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer' onClick={logout}>Logout</button>
+   </div>
+   <div className=' flex h-[calc(100vh-70px)]'>
+   <Sidebar/>
+   <Outlet/>
+   </div>
+   </>
   )
 }
 
