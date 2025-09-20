3431a6083b8dde94c8e66ec639063d9c77546407
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-20 17:48:20 +0530

    installed toast and added fetchblog function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,9 +9,11 @@ import Dashboard from './pages/admin/Dashboard'
 import Comments from './pages/admin/Comments'
 import Login from './components/admin/Login'
 import 'quill/dist/quill.snow.css'
+import {Toaster} from 'react-hot-toast'
 const App = () => {
   return (
     <div>
+      <Toaster/>
       <Routes>
         <Route path = '/' element = {<Home/>} />
         <Route path = '/blog/:id' element = {<Blog />} />
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import { Children, useContext, useState } from "react";
+import { Children, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { createContext, useNavigate } from "react-router-dom";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
@@ -10,6 +11,28 @@ export const AppProvider = ({Children})=>{
     const [token,setToken] = useState(null)
     const [blog,setBlogs] = useState([]);
     const [input ,setInput]  = useState('') //filter blogs
+    const fetchBlogs = async()=>{
+        try{
+            //already added baseurl
+            const {data} = await axios.get('/api/blog/all');
+            data.success ? setBlogs(data.blogs) : toast.error(data.message)
+        }
+        catch(error){
+            toast.error(error.message)
+        }
+    }
+    useEffect(()=>{
+        fetchBlogs()
+        //user auth
+        //when load web page chek the abailable token at browser local storage if available add in axios headers
+        const token = localStorage.getItem('token')
+        if(token){
+            setToken(token);
+            axios.defaults.headers.common['Authorization'] =`${token}`
+            //add when token available in all api cals
+        }
+
+    },[])
     const value={
         axios,navigate,token,setToken,blog,setBlogs,input,setInput
     }
