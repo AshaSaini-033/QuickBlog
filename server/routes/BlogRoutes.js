cbf37a2b63f537772251b8912f58e5584108118e
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-20 13:23:12 +0530

    added routes for alll blogId  delete toggle publish

--- server/routes/BlogRoutes.js
+++ server/routes/BlogRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addBlog } from '../controllers/blogController.js';
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublish } from '../controllers/blogController.js';
 import upload from '../middleware/multer.js';
 import auth from '../middleware/auth.js';
 
@@ -7,6 +7,11 @@ const blogRouter = express.Router();
 
 // add in req.file
 blogRouter.post('/add',upload.single('image') ,auth, addBlog)
+blogRouter.get('/all' ,getAllBlogs)
+blogRouter.get('/:blogId',getBlogById);
+// only admin can delet
+blogRouter.post('/delete',auth,deleteBlogById)
+blogRouter.post('/toggle-publish',auth,togglePublish);
 //need mw to parse image whivh upload from frontend use multer pacakge
 
 export default blogRouter;
