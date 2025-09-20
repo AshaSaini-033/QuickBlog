a45ce57881f7359d22dd03610e4cc7fb3194e2c6
Asha Saini <ashasainiasha0866@gmail.com>
2025-09-20 18:23:46 +0530

    added navbar funtionality to backend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
-    const navigate = useNavigate();
+   const {token ,navigate} = useAppContext();
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
       <img onClick={()=>navigate('/') } className='w-32 sm:w-44 hover:cursor-pointer' src ={assets.logo} alt ='logo' />
-      <button onClick={()=>navigate('/admin')} className=' hover:cursor-pointer flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-pink px-10 py-2.5'>Login
+      <button onClick={()=>navigate('/admin')} className=' hover:cursor-pointer flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-pink px-10 py-2.5'>{token ?'Dashboard' :'Login'}
         <img src ={assets.arrow} alt ='arrow'/>
       </button>
       
